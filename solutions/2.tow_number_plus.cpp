d2c70031d8cede66a23b2e41ddd0d49199b35532
2580553038 <2580553038@qq.com>
2026-01-13 20:13:22 +0800

    添加完善一道题目

---
+++ solutions/2.tow_number_plus.cpp
@@ -0,0 +1,39 @@
+﻿class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* ret=new ListNode();
+        ListNode* i=l1;
+        ListNode* l=l2;
+        ListNode* end = ret;
+        int jinwei=0;
+        while(i!=NULL||l!=NULL)
+        {
+            int num=jinwei;
+            if(i!=NULL)
+            {
+                num+=i->val;
+            }
+            if(l!=NULL)
+            {
+                num+=l->val;
+            }
+            jinwei=num/10;
+            num%=10;
+            ret->next=new ListNode(num);
+            ret=ret->next;
+            if(i!=NULL)
+            {
+                i=i->next;
+            }
+            if(l!=NULL)
+            {
+                l=l->next;
+            }
+        }
+        if(jinwei!=0)
+        {
+            ret->next=new ListNode(jinwei);
+        }
+        return end->next;
+    }
+};
